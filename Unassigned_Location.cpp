25415834f2118329238c676b48488a0ee399df90
Sharath <87656483+Sharath133@users.noreply.github.com>
2022-09-11 14:57:58 +0530

    Delete Unassigned_Location.cpp

--- Unassigned_Location.cpp
+++
@@ -1,9 +0,0 @@
-bool UnassignedLocation(int board[N][N],int& row, int& col)
-{
-    for (row = 0; row < N; row++)
-        for (col = 0; col < N; col++)
-            if (board[row][col] == UNASSIGNED)
-                return true;
-    return false;
-}
-}
